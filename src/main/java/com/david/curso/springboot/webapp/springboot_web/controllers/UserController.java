a01b047abb326c092f142c954aab744665f30dab
David MTZD <davidmtzd21512@gmail.com>
2024-09-12 15:15:10 -0600

    pasamos el titulo por medio de thymeleaf

--- src/main/java/com/david/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/david/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,14 +1,28 @@
 package com.david.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // componente que se maneja en la clase component
 public class UserController {
 
-    @GetMapping("/details")
-    public String details() {
+    /*
+     * @GetMapping("/details")
+     * public String details(Model model) {
+     * model.addAttribute("title", "Hola mundo con Spring Boot");
+     * model.addAttribute("name", "David");
+     * model.addAttribute("lastname", "MTZ");
+     * return "details";
+     * }
+     */
+    @GetMapping("/details") // se puede realizar con model o con map
+    public String details(Map<String, Object> model) {
 
-        return "index";
+        model.put("title", "Hola mundo con Spring Boot");
+        model.put("name", "David");
+        model.put("lastname", "MTZ");
+        return "details";
     }
 }
