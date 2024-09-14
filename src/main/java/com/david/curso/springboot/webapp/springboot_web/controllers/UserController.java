dc59aa52ba7e0020a07c04b61d64cf015cfba87e
David MTZD <davidmtzd21512@gmail.com>
2024-09-14 16:00:49 -0600

    se agrego el getmaping del listado clientes y se agrego el template list

--- src/main/java/com/david/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/david/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.david.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.david.curso.springboot.webapp.springboot_web.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // componente que se maneja en la clase component
 public class UserController {
@@ -27,4 +32,14 @@ public class UserController {
         model.put("user", user);
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+        model.addAttribute("title", "Listado de usuarios");
+        model.addAttribute("users", users);
+
+        return "list";
+    }
+
 }
