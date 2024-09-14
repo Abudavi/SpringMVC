61d79ea50a9fc9f9b0ca2f428a2bc996356d74d3
David MTZD <davidmtzd21512@gmail.com>
2024-09-14 17:31:20 -0600

    Se agrego un maping con multiples path variables

--- src/main/java/com/david/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/david/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.david.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/var")
@@ -17,6 +21,14 @@ public class PathVariableController {
         ParamDto paramDto = new ParamDto();
         paramDto.setMensage(message);
         return paramDto;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("producto", product);
+        json.put("id", id);
+        return json;
     }
 
 }
