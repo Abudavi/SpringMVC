591cb604e178e8233a50f5c2e206a41c95afe3cd
David MTZD <davidmtzd21512@gmail.com>
2024-09-16 16:17:25 -0600

    se quitaron impors que no se estaban usando

--- src/main/java/com/david/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/david/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,18 +1,16 @@
 package com.david.curso.springboot.webapp.springboot_web.controllers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.david.curso.springboot.webapp.springboot_web.models.User;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // componente que se maneja en la clase component
 public class UserController {
